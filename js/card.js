ffebcc07d077015da21d9e5799ad36e5f458f44c
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2019-11-04 17:54:53 -0300

    Iniciado a criação dos cards dos produtos

---
+++ js/card.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    // Lift card and show stats on Mouseover
+    $('.product-card').hover(function () {
+        $(this).addClass('animate');
+        $('div.carouselNext, div.carouselPrev').addClass('visible');
+    }, function () {
+        $(this).removeClass('animate');
+    });
+});
--- js/main.js
+++ js/main.js
@@ -19,5 +19,16 @@
         }
         
     });
-
 })(jQuery);
+
+$(document).ready(function(){
+	
+	// Lift card and show stats on Mouseover
+	$('.product-card').hover(function(){
+			$(this).addClass('animate');
+			$('div.carouselNext, div.carouselPrev').addClass('visible');			
+		 }, function(){
+			$(this).removeClass('animate');			
+			$('div.carouselNext, div.carouselPrev').removeClass('visible');
+    });	
+}
