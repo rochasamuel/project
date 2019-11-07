3692bbef0842e45c7d63b0925cd94b9d12bc96f5
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2019-11-07 16:57:26 -0300

    Formulario e footer completos

--- js/main.js
+++ js/main.js
@@ -77,3 +77,17 @@ $("#aces-link").click(function (event) {
         $('.acessorios').fadeIn();
     },300);
 });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
