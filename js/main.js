48bea439e6ff3068822aa4a34a8328cc402457d5
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2019-11-27 17:23:39 -0300

    ajustes rápidos

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-// (function ($) {
-//     "use strict"; 
-//  /*[ Fixed Header ]
-//     ===========================================================*/
-//     var header = $('header');
-//     var logo = $(header).find('.logo img');
-//     var linkLogo1 = $(logo).attr('src');
-//     var linkLogo2 = $(logo).data('logofixed');
-
-
-//     $(window).on('scroll',function(){
-//         if($(this).scrollTop() > 5 && $(this).width() > 992) {
-//             $(logo).attr('src',linkLogo2);
-//             $(header).addClass('header-fixed');
-//         }
-//         else {
-//             $(header).removeClass('header-fixed');
-//             $(logo).attr('src',linkLogo1);
-//         }
-
-//     });
-// })(jQuery);
 
 //ABA DE PRODUTOS//
 $("#desl-link").click(function (event) {
