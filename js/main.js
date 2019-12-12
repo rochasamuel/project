8b7198459831ce65b30f0ae2baa8e6a12cabe3dd
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2019-12-12 17:32:16 -0300

    Ajuste no js

--- js/main.js
+++ js/main.js
@@ -1,107 +1,126 @@
+$(function () {
+    abaProdutos();
+    scrollAncora();
+    dinamicMenu();
+    backToTop();
+    slider();
+})
 
-//ABA DE PRODUTOS//
-$("#desl-link").click(function (event) {
-    event.preventDefault();
-    $('.basculantes, .acessorios, .portas, .cancelas').fadeOut();
-    setTimeout(function () {
-        $('.deslizantes').fadeIn();
-    }, 300);
-    $('.product-card').removeClass("wow bounceInUp");
-    $('.product-card').removeAttr("data-wow-duration");
-    $('.product-card').removeAttr("style");
-});
-$("#basc-link").click(function (event) {
-    event.preventDefault();
-    $('.deslizantes, .acessorios, .portas, .cancelas').fadeOut();
-    setTimeout(function () {
-        $('.basculantes').fadeIn();
-    }, 300);
-});
-$("#canc-link").click(function (event) {
-    event.preventDefault();
-    $('.deslizantes, .basculantes, .portas, .acessorios').fadeOut();
-    setTimeout(function () {
-        $('.cancelas').fadeIn();
-    }, 300);
-});
-$("#port-link").click(function (event) {
-    event.preventDefault();
-    $('.deslizantes, .acessorios, .cancelas, .basculantes').fadeOut();
-    setTimeout(function () {
-        $('.portas').fadeIn();
-    }, 300);
-});
-$("#aces-link").click(function (event) {
-    event.preventDefault();
-    $('.deslizantes, .basculantes, .cancelas, .portas').fadeOut();
-    setTimeout(function () {
-        $('.acessorios').fadeIn();
-    }, 300);
-});
+function abaProdutos() {
+    //ABA DE PRODUTOS//
+    $("#desl-link").click(function (event) {
+        event.preventDefault();
+        $('.basculantes, .acessorios, .portas, .cancelas').fadeOut();
+        setTimeout(function () {
+            $('.deslizantes').fadeIn();
+        }, 300);
+        $('.product-card').removeClass("wow bounceInUp");
+        $('.product-card').removeAttr("data-wow-duration");
+        $('.product-card').removeAttr("style");
+    });
+    $("#basc-link").click(function (event) {
+        event.preventDefault();
+        $('.deslizantes, .acessorios, .portas, .cancelas').fadeOut();
+        setTimeout(function () {
+            $('.basculantes').fadeIn();
+        }, 300);
+    });
+    $("#canc-link").click(function (event) {
+        event.preventDefault();
+        $('.deslizantes, .basculantes, .portas, .acessorios').fadeOut();
+        setTimeout(function () {
+            $('.cancelas').fadeIn();
+        }, 300);
+    });
+    $("#port-link").click(function (event) {
+        event.preventDefault();
+        $('.deslizantes, .acessorios, .cancelas, .basculantes').fadeOut();
+        setTimeout(function () {
+            $('.portas').fadeIn();
+        }, 300);
+    });
+    $("#aces-link").click(function (event) {
+        event.preventDefault();
+        $('.deslizantes, .basculantes, .cancelas, .portas').fadeOut();
+        setTimeout(function () {
+            $('.acessorios').fadeIn();
+        }, 300);
+    });
 
-//SCROLL ANCORA//
-$('.menu a[href^="#"],.main-buttons a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
-    if (id == "#contato" || id == "#sobre") {
+}
+
+function scrollAncora() {
+    //SCROLL ANCORA//
+    $('.menu a[href^="#"],.main-buttons a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            targetOffset = $(id).offset().top;
+        if (id == "#contato" || id == "#sobre") {
+            $('html, body').animate({
+                scrollTop: targetOffset - 60
+            }, 500);
+        } else {
+            $('html, body').animate({
+                scrollTop: targetOffset - 90
+            }, 500);
+        }
+    });
+}
+
+function dinamicMenu() {
+    //MENU ATIVA COM SCROLL
+    $(window).on('scroll', function () {
+        var wintop = $(this).scrollTop();
+
+        if (wintop >= $('#intro').offset().top) {
+            $('.menu a[href^="#intro"]').addClass('active');
+            $('.menu a[href^="#sobre"], .menu a[href^="#produtos"], .menu a[href^="#contato"]').removeClass('active');
+        }
+        if (wintop >= $('#sobre').offset().top - 120) {
+            $('.menu a[href^="#sobre"]').addClass('active');
+            $('.menu a[href^="#intro"], .menu a[href^="#produtos"], .menu a[href^="#contato"]').removeClass('active');
+        }
+        if (wintop >= $('#produtos').offset().top - 150) {
+            $('.menu a[href^="#produtos"]').addClass('active');
+            $('.menu a[href^="#intro"], .menu a[href^="#sobre"], .menu a[href^="#contato"]').removeClass('active');
+        }
+        if (wintop >= ($('#contato').offset().top) - 100) {
+            $('.menu a[href^="#contato"]').addClass('active');
+            $('.menu a[href^="#intro"], .menu a[href^="#produtos"], .menu a[href^="#sobre"]').removeClass('active');
+        }
+    })
+
+}
+
+function backToTop() {
+    //BACK TO TOP APARECE COM SCROLL
+    $(window).on('scroll', function () {
+        var wintop = $(this).scrollTop();
+
+        if (wintop >= $('#sobre').offset().top) {
+            $('#back-to-top').fadeIn("slow");
+        } else {
+            $('#back-to-top').fadeOut("slow");
+        }
+    })
+
+    //VOLTA PRO TOPO AO CLIQUE
+    $('#back-to-top').click(function () {
         $('html, body').animate({
-            scrollTop: targetOffset - 60
-        }, 500);
-    }else{
-        $('html, body').animate({
-            scrollTop: targetOffset - 90
-        }, 500);
-    }
-});
+            scrollTop: 0
+        }, 600);
+    })
+}
 
-
-//MENU ATIVA COM SCROLL
-$(window).on('scroll', function(){
-    var wintop = $(this).scrollTop();
-
-    if(wintop >= $('#intro').offset().top){
-        $('.menu a[href^="#intro"]').addClass('active');
-        $('.menu a[href^="#sobre"], .menu a[href^="#produtos"], .menu a[href^="#contato"]').removeClass('active');
-    }
-    if(wintop >= $('#sobre').offset().top - 120){
-        $('.menu a[href^="#sobre"]').addClass('active');
-        $('.menu a[href^="#intro"], .menu a[href^="#produtos"], .menu a[href^="#contato"]').removeClass('active');
-    }
-    if(wintop >= $('#produtos').offset().top - 150){
-        $('.menu a[href^="#produtos"]').addClass('active');
-        $('.menu a[href^="#intro"], .menu a[href^="#sobre"], .menu a[href^="#contato"]').removeClass('active');
-    }
-    if(wintop >= ($('#contato').offset().top)-100){
-        $('.menu a[href^="#contato"]').addClass('active');
-        $('.menu a[href^="#intro"], .menu a[href^="#produtos"], .menu a[href^="#sobre"]').removeClass('active');
-    }
-})
-
-//BACK TO TOP APARECE COM SCROLL
-$(window).on('scroll', function(){
-    var wintop = $(this).scrollTop();
-
-    if(wintop >= $('#sobre').offset().top){
-        $('#back-to-top').fadeIn("slow");
-    }else{
-        $('#back-to-top').fadeOut("slow");
-    }
-})
-
-//VOLTA PRO TOPO AO CLIQUE
-$('#back-to-top').click(function(){
-    $('html, body').animate({
-        scrollTop: 0
-    }, 600);
-})
-
-//SLICK
-$('.variable-width').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
-});
+function slider() {
+    //SLICK
+    $('.variable-width').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true
+    });
+}
